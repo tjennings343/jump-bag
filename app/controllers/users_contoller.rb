ab9ea944b2d5b20a97e985aa5fcb0929a5d19a88
tjennings343 <tjennings343@gmail.com>
2020-12-06 22:44:00 -0500

    added homepage route and logout

--- app/controllers/users_contoller.rb
+++ app/controllers/users_contoller.rb
@@ -5,29 +5,39 @@ class UsersController < ApplicationController
     end
 
     post "/login" do
-        # find user
         @user = User.find_by(username: params[:username])
-        # authenticate user
         if @user.authenticate(params[:password])
             session[:id] = @user.id
             redirect "users/#{@user.id}"
         else
-            
+            redirect "/users/signup"
         end
-        # log in
-        # redirect to user page
-    end
-
-    get "/users/:id" do
-        "this is the user page"
+        
     end
 
     get "/users/signup" do
         erb :"/users/signup"
     end
 
-    post "/users/signup" do 
-        @user = User.create(:name => params[:name], :username => params[:username], :password => params[:password])        
+    post "/users" do 
+        if params[:name] != "" && params[:username] != "" && params[:password] != ""
+            @user = User.create(:name => params[:name], :username => params[:username], :password => params[:password])
+            session[:id] = @user.id
+            redirect "/users/#{@user.id}"
+        else
+
+        end
+
+    end
+
+    get "/users/:id" do
+        @user = User.find_by(id: params[:id])
+        erb :"/users/homepage"    
+    end
+
+    get "/logout" do
+        session.clear
+        redirect "/"
     end
 
 
